aa035198f2ac520a1f49bd0f0a9b6f98fa148047
DrumJ <tmdgh717@naver.com>
2025-10-27 15:49:09 +0900

    feat. 오브젝트 팩토리

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -6,7 +6,9 @@ import java.net.URISyntaxException;
 
 public class Client {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
+
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
