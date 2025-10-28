5767641f621bde24f05363a533b356e66f84561c
DrumJ <tmdgh717@naver.com>
2025-10-29 08:57:16 +0900

    feat. 구성정보를 가져오는 다른 방법

--- src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/WebApiExRateProvider.java
@@ -1,6 +1,7 @@
 package tobyspring.hellospring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,6 +13,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.stream.Collectors;
 
+@Component
 public class WebApiExRateProvider implements ExRateProvider {
 
     @Override
