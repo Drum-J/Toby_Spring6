136755add70febf4cff592ff7eb92a4276bf32fc
DrumJ <tmdgh717@naver.com>
2025-10-28 09:15:23 +0900

    feat. 원칙과 패턴

---
+++ src/main/java/tobyspring/hellospring/Sort.java
@@ -0,0 +1,20 @@
+package tobyspring.hellospring;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Sort {
+    public static void main(String[] args) {
+
+        List<Integer> scores = Arrays.asList(5, 7, 1, 9, 2, 8);
+        Collections.sort(scores);
+
+        scores.forEach(System.out::println);
+
+        List<String> strings = Arrays.asList("z", "x", "spring", "java");
+        strings.sort((o1, o2) -> o1.length() - o2.length());
+
+        strings.forEach(System.out::println);
+    }
+}
