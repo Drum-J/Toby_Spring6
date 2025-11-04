a39e81c630f7f64aba01641853ae22d350c5bc53
DrumJ <tmdgh717@naver.com>
2025-11-04 09:39:39 +0900

    feat. JPA를 이용한 Order 저장

---
+++ src/main/java/tobyspring/hellospring/DataConfig.java
@@ -0,0 +1,37 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.sql.DataSource;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
+
+@Configuration
+public class DataConfig {
+    // data source
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(H2).build();
+    }
+
+    // entity manager factory
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
+
+        emf.setDataSource(dataSource());
+        emf.setPackagesToScan("tobyspring.hellospring");
+        emf.setJpaVendorAdapter(new HibernateJpaVendorAdapter() {{
+            setDatabase(Database.H2);
+            setGenerateDdl(true);
+            setShowSql(true);
+        }});
+
+        return emf;
+    }
+}
