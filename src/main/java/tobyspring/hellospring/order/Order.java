d1b9df1df540279543cae77f4a58dfeeddd3de62
DrumJ <tmdgh717@naver.com>
2025-11-05 09:19:03 +0900

    feat. DataAccessException과 예외 추상화

--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -10,7 +10,9 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Table(name = "orders")
 public class Order {
 
-    @Id @GeneratedValue(strategy = IDENTITY)
+    // GeneratedValue의 strategy 를 IDENTITY로 설정 시 여전히 org.hibernate.exception.ConstraintViolationException 발생
+    // 해당 strategy를 설정하지 않은 경우 DataIntegrityViolationException 발생
+    @Id @GeneratedValue//(strategy = IDENTITY)
     private Long id;
 
     @Column(unique = true)
