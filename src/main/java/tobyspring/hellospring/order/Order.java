a0a3ab4d2eb8a1795942fa0192a68213f1260296
DrumJ <tmdgh717@naver.com>
2025-12-04 09:42:08 +0900

    feat. 기술에 독립적인 애플리케이션 서비스

--- src/main/java/tobyspring/hellospring/order/Order.java
+++ src/main/java/tobyspring/hellospring/order/Order.java
@@ -6,16 +6,16 @@ import java.math.BigDecimal;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
-@Entity
-@Table(name = "orders")
+//@Entity
+//@Table(name = "orders")
 public class Order {
 
     // GeneratedValue의 strategy 를 IDENTITY로 설정 시 여전히 org.hibernate.exception.ConstraintViolationException 발생
     // 해당 strategy를 설정하지 않은 경우 DataIntegrityViolationException 발생
-    @Id @GeneratedValue//(strategy = IDENTITY)
+    //@Id @GeneratedValue//(strategy = IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    //@Column(unique = true)
     private String no;
 
     private BigDecimal total;
