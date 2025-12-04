ddc787b334b9aeb07cd840dc382b30e5f13b8056
DrumJ <tmdgh717@naver.com>
2025-12-04 13:54:40 +0900

    feat. 트랜잭션 테스트

--- src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
+++ src/test/java/tobyspring/hellospring/order/OrderServiceSpringTest.java
@@ -4,11 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 import tobyspring.hellospring.OrderConfig;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -17,6 +22,8 @@ import static org.assertj.core.api.Assertions.*;
 public class OrderServiceSpringTest {
 
     @Autowired OrderService orderService;
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void createOrder() throws Exception {
@@ -27,5 +34,40 @@ public class OrderServiceSpringTest {
 
         //then
         assertThat(order.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void createOrders() throws Exception {
+        //given
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0200", BigDecimal.ONE),
+                new OrderReq("0201", BigDecimal.TWO)
+        );
+
+        //when
+        var orders = orderService.createOrders(orderReqs);
+
+        //then
+        assertThat(orders).hasSize(2);
+        orders.forEach(order -> assertThat(order.getId()).isGreaterThan(0));
+    }
+
+    @Test
+    void createDuplicatedOrders() throws Exception {
+        //given
+        List<OrderReq> orderReqs = List.of(
+                new OrderReq("0300", BigDecimal.ONE),
+                new OrderReq("0300", BigDecimal.TWO) // 주문 번호가 동일해서 예외 발생 유도
+        );
+
+        //when : 예외 발생 확인!
+        assertThatThrownBy(() -> orderService.createOrders(orderReqs))
+                .isInstanceOf(DataIntegrityViolationException.class);
+
+        //then : 그러면 (0300, ONE) 인 Order 는 롤백이 제대로 되었는가?
+        JdbcClient client = JdbcClient.create(dataSource);
+        var count = client.sql("select count(*) from orders where no = '0300'").query(Long.class).single();
+
+        assertThat(count).isEqualTo(0); // TransactionTemplate을 사용했을 때 정상적으로 롤백된다.
     }
 }
